8be82ccfdfdab12928619d5b317694229f9d3ff6
Asami-hello <imada.asami@gmail.com>
2025-07-21 15:36:47 +0900

    Laravel課題5の提出

--- laravel-basic-kadai/routes/web.php
+++ laravel-basic-kadai/routes/web.php
@@ -18,6 +18,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+
+Route::get('/posts/create', [PostController::class, 'create']);
 
 Route::get('/posts/{id}', [PostController::class, 'show']);
+
+Route::post('/posts/store', [PostController::class, 'store'])->name('posts.store');
+
